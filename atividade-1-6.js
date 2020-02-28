c69989d74cc1a853b7e03fbb9f7f42fc0dc1d9a0
Gabriel Alves <gabriel.alves450@hotmail.com>
2020-02-27 21:55:45 -0300

    Atividade de IF.

--- atividade-1-6.js
+++ atividade-1-6.js
@@ -5,20 +5,14 @@ var n2 = parseInt(user.question("Escreva outro numero "))
 var n3 = parseInt(user.question("Escreva mais um numero "))
 
 var valorTotal = null
-if (n1 < n2 && n3) {
+if(n1 >= n2 && n3)
+{
   console.log(n1)
-  if (n2 < n3)
-    console.log(n2)
+  console.log(n2)
   console.log(n3)
-}
-if (n1 > n2 && n3){
- if(n2>n3){
-   console.log(n3)
-   console.log(n2)
-   console.log(n1)
- }else{
-   console.log(n2)
-   console.log(n3)
-   console.log(n1)
- }
+}else if (n2 >= n1 && n3)
+{
+  console.log(n1)
+  console.log(n1)
+  console.log(n1)
 }
